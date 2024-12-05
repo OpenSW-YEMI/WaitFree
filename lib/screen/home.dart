57996dde830d1f4f819917e2ce6a6ef78d659fc6
combikms <combikms@naver.com>
2024-12-05 19:38:19 +0900

    :bug: Modify: 홈 화면에서 뒤로 가기시 로그인화면으로 돌아가는 현상

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -15,7 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -59,59 +58,73 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent, // 배경을 투명하게 설정
-        elevation: 0,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            color: Colors.white, // AppBar의 배경색 설정
+    return WillPopScope(
+      onWillPop: () async {
+        if (_selectedIndex != 0) {
+          // 홈 화면이 아닌 다른 화면일 경우 홈 화면으로 돌아가도록 함
+          setState(() {
+            _selectedIndex = 0; // 홈 화면 인덱스로 설정
+          });
+          return false; // 기본 뒤로가기 동작을 차단
+        }
+        // 홈 화면이면 앱 종료
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent, // 배경을 투명하게 설정
+          elevation: 0,
+          automaticallyImplyLeading: false, // 뒤로가기 버튼 제거 (원하는 경우)
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              color: Colors.white, // 스크롤 시에도 흰색 배경
+            ),
+          ),
+          centerTitle: true,
+          title: Text(
+            '웨잇프리',
+            style: TextStyle(
+              color: Colors.teal[200],
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
           ),
         ),
-        centerTitle: true,
-        title: Text(
-          '웨잇프리',
-          style: TextStyle(
-            color: Colors.teal[200],
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-      ),
 
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.teal,
-        unselectedItemColor: Colors.teal[200],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/icon/icon_search.png'), size: 24),
-            label: '둘러보기',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/icon/icon_calendar.png'), size: 24),
-            label: '예약정보',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/icon/icon_heart.png'), size: 24),
-            label: '찜',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/icon/icon_bell.png'), size: 24),
-            label: '알림',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/icon/icon_person.png'), size: 24),
-            label: '정보',
-          ),
-        ],
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _pages,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.teal,
+          unselectedItemColor: Colors.teal[200],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const [
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icon/icon_search.png'), size: 24),
+              label: '둘러보기',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icon/icon_calendar.png'), size: 24),
+              label: '예약정보',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icon/icon_heart.png'), size: 24),
+              label: '찜',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icon/icon_bell.png'), size: 24),
+              label: '알림',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/icon/icon_person.png'), size: 24),
+              label: '정보',
+            ),
+          ],
+        ),
       ),
     );
   }
